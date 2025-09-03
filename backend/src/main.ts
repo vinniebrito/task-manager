242fc1922421af577a68e6ec377028b495003410
vnnbrt <vinniebritos@gmail.com>
2025-09-03 20:56:45 -0300

    feat: configure CORS and global validation pipes in main application

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3001' });
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

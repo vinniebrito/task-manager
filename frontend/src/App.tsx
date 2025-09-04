d1b7717c45be5e4816aef3e6688399ecb4f80583
vnnbrt <vinniebritos@gmail.com>
2025-09-04 19:47:56 -0300

    feat: integrate Header component with search state management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Header from "./components/organisms/Header";
+
 function App() {
+  const [search, setSearch] = React.useState("");
+
   return (
     <>
-      <p>Teste</p>
+      <Header search={search} setSearch={setSearch} />
     </>
   );
 }

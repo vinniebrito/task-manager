f04c20767dec15ea70caa855006f04c09844b6ef
vnnbrt <vinniebritos@gmail.com>
2025-09-05 13:03:27 -0300

    feat: add compactOnMobile prop to PrimaryButton and update styles for disabled state

--- frontend/src/components/atoms/buttons/PrimaryButton.tsx
+++ frontend/src/components/atoms/buttons/PrimaryButton.tsx
@@ -2,6 +2,8 @@ type PrimaryButtonProps = {
   children?: React.ReactNode;
   icon?: React.ReactNode;
   className?: string;
+  disabled?: boolean;
+  compactOnMobile?: boolean; // nova prop
   [key: string]: any;
 };
 
@@ -9,15 +11,33 @@ const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   children,
   icon,
   className = "",
+  disabled = false,
+  compactOnMobile = false,
   ...props
 }) => (
   <button
-    className={`flex items-center gap-2 px-4 rounded-[8px] bg-root text-cream hover:bg-limewash transition cursor-pointer h-8 shadow 
+    className={`flex items-center gap-2 px-4 rounded-[8px] bg-root text-cream transition shadow
+      h-8
+      ${compactOnMobile ? "sm:h-8 h-8 sm:px-4 px-2" : ""}
+      ${
+        disabled
+          ? "opacity-50 cursor-not-allowed bg-sand text-root"
+          : "hover:bg-limewash cursor-pointer"
+      }
       ${className}`}
+    disabled={disabled}
     {...props}
   >
     {icon}
-    <span className="hidden sm:inline whitespace-nowrap ">{children}</span>
+    <span
+      className={
+        compactOnMobile
+          ? "hidden sm:inline whitespace-nowrap"
+          : "inline whitespace-nowrap"
+      }
+    >
+      {children}
+    </span>
   </button>
 );
 

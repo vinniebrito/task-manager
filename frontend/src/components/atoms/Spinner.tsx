c96c42af2113a62c2112f2aebd1b73ba640f5b23
vnnbrt <vinniebritos@gmail.com>
2025-09-05 13:04:42 -0300

    feat: add Spinner component and update AddIcon to accept dynamic fill color

---
+++ frontend/src/components/atoms/Spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex justify-center items-center py-4">
+    <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-root"></div>
+  </div>
+);
+
+export default Spinner;
--- frontend/src/components/atoms/icons/Add.tsx
+++ frontend/src/components/atoms/icons/Add.tsx
@@ -4,7 +4,7 @@ const AddIcon = (props: React.SVGProps<SVGSVGElement>) => (
     height="24px"
     viewBox="0 -960 960 960"
     width="24px"
-    fill="#FFFFFF"
+    fill={props.fill ?? "#FFFFFF"}
     {...props}
   >
     <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />

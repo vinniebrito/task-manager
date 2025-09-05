50e10a75ef512d3972c9b723bd2131b9fe622015
vnnbrt <vinniebritos@gmail.com>
2025-09-05 14:11:22 -0300

    feat: add Footer component with copyright notice

---
+++ frontend/src/components/organisms/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="w-full flex justify-between gap-6 items-center px-7 py-4 border-t border-gray-300 bg-oat">
+      <p className="text-sm text-gray-500 text-center w-full">
+        © 2025 DONE!. Todos os direitos reservados.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
